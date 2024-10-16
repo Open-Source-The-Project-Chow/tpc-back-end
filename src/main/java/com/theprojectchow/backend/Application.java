ad82daaa8c92f3fa19ba643febc150cd09562bfd
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-10-16 12:46:37 -0500

    chore: updated application properties

--- src/main/java/com/theprojectchow/backend/Application.java
+++ src/main/java/com/theprojectchow/backend/Application.java
@@ -2,8 +2,10 @@ package com.theprojectchow.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Enable JPA Auditing
 public class Application {
 
 	public static void main(String[] args) {
